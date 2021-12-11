3412fe5d5cf27d29d445f3e2c0ac77b5d431ae93
Luisa Huanca <luhuanca.c03@ucamp.utel.edu.mx>
2021-12-11 13:51:21 -0500

    Tarjetas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Tarjeta } from './components/Tarjeta';
+import "./style.css";
 
 const App = () => {
   return (
     <>
-      App
+    <section className='container'>
+      <Tarjeta title1='Capitana'  src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/capitana-marvel-poster-1551691489.jpg?crop=1xw:1xh;center,top&resize=480:*"/>
+      <Tarjeta title1='Vengadores' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-endgame-orden-peli-culas-marvel-1556096718.jpg?crop=1xw:1xh;center,top&resize=480:*"/>
+      <Tarjeta title1='Guardianes' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/guardianes-de-la-galaxia-1525787553.jpg?crop=1xw:1xh;center,top&resize=480:*"/>
+      <Tarjeta title1='Vengadores 2' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-era-de-ultron-poster-1551691493.jpg?crop=1xw:1xh;center,top&resize=480:*"/>
+      <Tarjeta title1='Spiderman' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-homecoming-poster-1551691492.jpg?crop=1xw:1xh;center,top&resize=480:*"/>
+      <Tarjeta title1='Thor 3' src="https://es.web.img2.acsta.net/pictures/17/08/25/09/36/103601.jpg"/>
+    </section>
     </>
+
+  
   )
 }
 
